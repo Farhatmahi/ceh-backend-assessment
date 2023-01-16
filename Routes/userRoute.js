848fd368a2848e88afc8a849a196380e62f87da0
Farhat Mahi <farhatmahi007@gmail.com>
2023-01-16 15:21:49 +0600

    Registration api fixed and login api

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router()
-const {register} = require('../Controllers/userController');
+const {register, login} = require('../Controllers/userController');
 
 
 router.route('/register').post(register)
-// router.route('/login').post(login)
+router.route('/login').post(login)
 
 
 module.exports = router
