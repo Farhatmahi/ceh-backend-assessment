701a395c8a8ba230ae2a13f0f886759ac49861fa
Farhat Mahi <farhatmahi007@gmail.com>
2023-01-16 14:03:51 +0600

    Setting up mongoose environment

--- index.js
+++ index.js
@@ -5,176 +5,210 @@ require("dotenv").config();
 const bcrypt = require("bcrypt");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 //middleware
 app.use(express.json());
 
-//jwt middleware
-const verifyJWT = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    res.status(401).send({ message: "Unauthorized access" });
-  }
+// //jwt middleware
+// const verifyJWT = async (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     res.status(401).send({ message: "Unauthorized access" });
+//   }
 
-  const token = authHeader.split(" ")[1];
+//   const token = authHeader.split(" ")[1];
 
-  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-    if (err) {
-      res.status(403).send({ message: "Forbidden access" });
-    }
-    req.decoded = decoded;
-    next();
-  });
-};
+//   jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+//     if (err) {
+//       res.status(403).send({ message: "Forbidden access" });
+//     }
+//     req.decoded = decoded;
+//     next();
+//   });
+// };
 
-app.get("/", async (req, res) => {
-  res.send("Server is running");
-});
+// app.get("/", async (req, res) => {
+//   res.send("Server is running");
+// });
 
-const uri = `mongodb+srv://farhatmahi:Farhat007@cluster0.6ulnnbw.mongodb.net/test?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
+// const uri = `mongodb+srv://farhatmahi:Farhat007@cluster0.6ulnnbw.mongodb.net/test?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1,
+// });
 
-const run = async () => {
-  try {
-    const usersCollection = client.db("CEH").collection("users");
-    const projectsCollection = client.db("CEH").collection("projects");
-    const reportsCollection = client.db("CEH").collection("reports");
+// const run = async () => {
+//   try {
+//     const usersCollection = client.db("CEH").collection("users");
+//     const projectsCollection = client.db("CEH").collection("projects");
+//     const reportsCollection = client.db("CEH").collection("reports");
+//     const requestCollection = client.db("CEH").collection("request");
 
-    //registration api
-    app.post("/registration", async (req, res) => {
-      const { username, email, password, profilePicture, role } = req.body;
+//     //registration api
+//     app.post("/registration", async (req, res) => {
+//       const { username, email, password, profilePicture, role } = req.body;
 
-      const existingUser = await usersCollection.findOne({
-        $or: [{ username: username }, { email: email }],
-      });
-      if (existingUser) {
-        return res
-          .status(400)
-          .json({ message: "Username or email already in use" });
-      }
+//       const existingUser = await usersCollection.findOne({
+//         $or: [{ username: username }, { email: email }],
+//       });
+//       if (existingUser) {
+//         return res
+//           .status(400)
+//           .json({ message: "Username or email already in use" });
+//       }
 
-      const hashedPassword = await bcrypt.hash(password, 10);
+//       const hashedPassword = await bcrypt.hash(password, 10);
 
-      const user = {
-        username,
-        email,
-        password: hashedPassword,
-        profilePicture,
-        role,
-      };
+//       const user = {
+//         username,
+//         email,
+//         password: hashedPassword,
+//         profilePicture,
+//         role,
+//       };
 
-      const result = await usersCollection.insertOne(user);
-      //   console.log(result);
-      if (result.acknowledged) {
-        const token = jwt.sign({ username }, process.env.ACCESS_TOKEN);
-        res.send({ token });
-      }
-    });
+//       const result = await usersCollection.insertOne(user);
+//       //   console.log(result);
+//       if (result.acknowledged) {
+//         const token = jwt.sign({ username }, process.env.ACCESS_TOKEN);
+//         res.send({ token });
+//       }
+//     });
 
-    //login api
-    app.post("/login", async (req, res) => {
-      const { username, password } = req.body;
-      const user = await usersCollection.findOne({ username: username });
+//     //login api
+//     app.post("/login", async (req, res) => {
+//       const { username, password } = req.body;
+//       const user = await usersCollection.findOne({ username: username });
 
-      if (!user) {
-        res.status(401).send({ message: "Invalid credentials" });
-      }
+//       if (!user) {
+//         res.status(401).send({ message: "Invalid credentials" });
+//       }
 
-      if (bcrypt.compareSync(password, user?.password)) {
-        const token = jwt.sign({ username }, process.env.ACCESS_TOKEN);
-        res.send({ token });
-      } else {
-        res.status(401).json({ message: "Invalid credentials" });
-      }
-    });
+//       if (bcrypt.compareSync(password, user?.password)) {
+//         const token = jwt.sign({ username }, process.env.ACCESS_TOKEN);
+//         res.send({ token });
+//       } else {
+//         res.status(401).json({ message: "Invalid credentials" });
+//       }
+//     });
 
-    //add project
-    app.post("/add-project", verifyJWT, async (req, res) => {
-      const project = req.body;
-      const findClient = await usersCollection.findOne({
-        username: project?.client_username,
-      });
+//     //add project
+//     app.post("/add-project", verifyJWT, async (req, res) => {
+//       const project = req.body;
+//       const findClient = await usersCollection.findOne({
+//         username: project?.client_username,
+//       });
 
-      if (findClient) {
-        const result = await projectsCollection.insertOne(project);
-        res.status(200).send(result);
-      } else {
-        res.status(404).send({ message: "Client not found" });
-      }
-    });
+//       if (findClient) {
+//         const result = await projectsCollection.insertOne(project);
+//         res.status(200).send(result);
+//       } else {
+//         res.status(404).send({ message: "Client not found" });
+//       }
+//     });
 
-    //add report
-    app.post("/add-report/:id", verifyJWT, async (req, res) => {
-      const projectId = req.params.id;
-      const project = await projectsCollection.findOne({
-        _id: ObjectId(projectId),
-      });
+//     //add report
+//     app.post("/add-report/:id", verifyJWT, async (req, res) => {
+//       const projectId = req.params.id;
+//       const project = await projectsCollection.findOne({
+//         _id: ObjectId(projectId),
+//       });
 
-      if (!project) {
-        return res.status(404).send({ message: "Project not found" });
-      }
+//       if (!project) {
+//         return res.status(404).send({ message: "Project not found" });
+//       }
 
-      const newReport = req.body;
+//       const newReport = req.body;
 
-      if (newReport.week === "" || newReport.report_link === "") {
-        return res.status(400).send({ message: "Please enter week and report" });
-      } else {
-        const result = await reportsCollection.insertOne(newReport);
-        res.status(200).send(result);
-      }
-    });
+//       if (newReport.week === "" || newReport.report_link === "") {
+//         return res
+//           .status(400)
+//           .send({ message: "Please enter week and report" });
+//       } else {
+//         const result = await reportsCollection.insertOne(newReport);
+//         res.status(200).send(result);
+//       }
+//     });
 
-    //add feedback
-    app.put("/add-feedback/:id", async (req, res) => {
-      const projectId = req.params.id;
-      const project = await projectsCollection.findOne({
-        _id: ObjectId(projectId),
-      });
+//     //add feedback
+//     app.put("/add-feedback/:id", async (req, res) => {
+//       const projectId = req.params.id;
+//       const project = await projectsCollection.findOne({
+//         _id: ObjectId(projectId),
+//       });
 
-      if (!project) {
-        return res.status(404).send({ message: "Project not found" });
-      }
+//       if (!project) {
+//         return res.status(404).send({ message: "Project not found" });
+//       }
 
-      if(project.feedback){
-        return res.send({message : "Feedback already added"})
-      }
+//       if (project.feedback) {
+//         return res.send({ message: "Feedback already added" });
+//       }
 
-      const newFeedback = req.body;
+//       const newFeedback = req.body;
 
-      if (!newFeedback.stars === "undefined") {
-        return res.status(400).send({ message: "Please leave stars" });
-      }
+//       if (!newFeedback.stars === "undefined") {
+//         return res.status(400).send({ message: "Please leave stars" });
+//       }
 
-      const updatedDoc = {
-        $set: {
-          feedback: newFeedback,
-        },
-      };
+//       const updatedDoc = {
+//         $set: {
+//           feedback: newFeedback,
+//         },
+//       };
+
+//       const result = await projectsCollection.updateOne(project, updatedDoc, {
+//         upsert: true,
+//       });
+//       res.send(result);
+//     });
+
+//     //student request api
+//     app.post("/v1/request", async (req, res) => {
+//       const student = req.body;
+
+//       if (
+//         student.name === "" ||
+//         student.email === "" ||
+//         student.phone_number === "" ||
+//         student.address === ""
+//       ) {
+//         return res.send({ message: "Please enter all the data" });
+//       }
+
+//       const request = await requestCollection.insertOne(student)
 
 
-      const result = await projectsCollection.updateOne(project, updatedDoc, {
-        upsert: true,
-      });
-      res.send(result);
-
-    });
+//     });
 
 
+//   } catch (error) {
+//     res.status(400);
+//   }
+// };
 
-
-  } catch (error) {
-    res.status(400);
-  }
-};
-
-run().catch((err) => {
-  console.log(err);
-});
+// run().catch((err) => {
+//   console.log(err);
+// });
 
 app.listen(port, () => {
   console.log(`Server is running on port : ${port}`);
 });
+
+
+mongoose.set("strictQuery", true);
+const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+connectDB().catch(err => console.log(err))
